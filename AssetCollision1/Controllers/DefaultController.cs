7570cc232b5924cea7330f34005f69028738e67d
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-03-11 09:28:16 +0000

    Add projects to repro asset class collision

---
+++ AssetCollision1/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetCollision1.Controllers
+{
+    public partial class DefaultController : Controller
+    {
+        //
+        // GET: /Default/
+
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
